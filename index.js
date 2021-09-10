55f56263d32c07deaf5da8dbe11040691e022d01
Nguyen Cong Thanh <muahangonlineabc@gmail.com>
2021-09-10 07:42:41 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  var a = 3;
+  var b = 4;
+
+  var c = a + b;
+  res.send("abc");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
